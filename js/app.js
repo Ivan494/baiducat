f4916d47b409d951551e172078cfa8a134462ac3
OPHaru <wuchitho1@hotmail.com>
2018-02-07 12:33:00 +0800

    auto open tab, get captcha

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 var apiQueryPetsOnSale = 'https://pet-chain.baidu.com/data/market/queryPetsOnSale';
 var apiTxnCreate = 'https://pet-chain.baidu.com/data/txn/create';
 
-function getBaiduDogs(degreeConf) {
+function getBaiduDogs(degreeConf,intervalObj) {
     // 等级配置
     //var degreeConf = options.getDegreeConf();
     $.ajax({
@@ -27,6 +27,7 @@ function getBaiduDogs(degreeConf) {
             var petsOnSale = res.data.petsOnSale || [];
             console.clear();
             console.table(res.data.petsOnSale);
+            $('.sort span:nth-child(2)').trigger("click");
 
             $.each(petsOnSale, function (index, item) {
                 var degree = degreeConf[item.rareDegree] || { desc: '未知', buyAmount: 5 };
@@ -34,37 +35,38 @@ function getBaiduDogs(degreeConf) {
                 if (parseFloat(item.amount) <= parseFloat(buyAmount)) {
                     var paddedid = pad_with_zeroes(item.id);
                     $('.dog-wrapper h3 span:nth-child(2)').each(function () {
-                        if(paddedid==$(this).html()){
-                            console.log(paddedid);
+                        if (paddedid == $(this).html()) {
+                            //console.log(paddedid);
                             $(this).trigger("click");
-                            console.log($(".sms-img img").attr('src'));
+/*                             console.log($(".sms-img img").attr('src'));
+                            clearInterval(intervalObj); */
                         }
-/*                         var catId = $('h3 span:nth-child(2)').html(); */
-/*                         if (catId == paddedid) {
-                            console.log(catId);
-                        } */
+                        /*                         var catId = $('h3 span:nth-child(2)').html(); */
+                        /*                         if (catId == paddedid) {
+                                                    console.log(catId);
+                                                } */
                     });
-/*                     $.ajax({
-                        type: 'POST',
-                        url: apiTxnCreate,
-                        contentType: 'application/json',
-                        data: JSON.stringify({
-                            "petId": item.petId,
-                            "requestId": new Date().getTime(),
-                            "amount": item.amount,
-                            "appId": 1,
-                            "tpl": ""
-                        }),
-                        success: function (res2) {
-                            console.log("尝试购买：ID[" + item.id + "],级别[" + item.rareDegree + "],价格[" + item.amount + ']')
-                            console.log("命中策略：等级[" + degree.desc + "],最高价格[" + degree.buyAmount + ']')
-                            if (res2.errorNo == 0) {
-                                console.log("抢到啦！！！！！")
-                            } else {
-                                console.log("没抢到：错误码[" + res2.errorNo + '],错误信息[' + res2.errorMsg + ']')
-                            }
-                        }
-                    }); */
+                    /*                     $.ajax({
+                                            type: 'POST',
+                                            url: apiTxnCreate,
+                                            contentType: 'application/json',
+                                            data: JSON.stringify({
+                                                "petId": item.petId,
+                                                "requestId": new Date().getTime(),
+                                                "amount": item.amount,
+                                                "appId": 1,
+                                                "tpl": ""
+                                            }),
+                                            success: function (res2) {
+                                                console.log("尝试购买：ID[" + item.id + "],级别[" + item.rareDegree + "],价格[" + item.amount + ']')
+                                                console.log("命中策略：等级[" + degree.desc + "],最高价格[" + degree.buyAmount + ']')
+                                                if (res2.errorNo == 0) {
+                                                    console.log("抢到啦！！！！！")
+                                                } else {
+                                                    console.log("没抢到：错误码[" + res2.errorNo + '],错误信息[' + res2.errorMsg + ']')
+                                                }
+                                            }
+                                        }); */
                 }
             });
         }
--- js/content.js
+++ js/content.js
@@ -1,49 +1,66 @@
 ﻿// 接收popup.js的消息
 chrome.extension.onMessage.addListener(
-  	function(request, sender, sendResponse) {
-    	if(request.cmd == "run") {
+	function (request, sender, sendResponse) {
+		if (request.cmd == "run") {
 			run();
-			sendResponse({status:1}); 
+			sendResponse({ status: 1 });
 		}
+	});
+$(document).ready(function () {
+	console.log('?');
+	//$('.detail').change(function(){
+
+	$('.button:visible').trigger('click');
+	/* 	console.log($(".sms-img img").attr('src'));
+		$(".sms-img img").change(function (e) {
+			console.log($(".sms-img img").attr('src'));
+		}); */
+	setInterval(scanCode, 3000);
+	//});
 });
 
-function run(){
+function scanCode() {
+	console.log($(".sms-img img").attr('src'));
+}
+
+function run() {
 	var defaultDegreeConf = [{
-	        desc: '普通',
-	        buyAmount: 100,
-	        saleAmount: 0,
-	    },{
-	        desc: '稀有',
-	        buyAmount: 100,
-	        saleAmount: 0,
-	    },{
-	        desc: '卓越',
-	        buyAmount: 100,
-	        saleAmount: 0,
-	    },{
-	        desc: '史诗',
-	        buyAmount: 100,
-	        saleAmount: 0,
-	    },{
-	        desc: '神话',
-	        buyAmount: 100,
-	        saleAmount: 0,
-	    },{
-	        desc: '传说',
-	        buyAmount: 100,
-	        saleAmount: 0,
-	    }];
-	
-	chrome.extension.sendMessage({cmd:"get_config"}, function (response) {
+		desc: '普通',
+		buyAmount: 100,
+		saleAmount: 0,
+	}, {
+		desc: '稀有',
+		buyAmount: 100,
+		saleAmount: 0,
+	}, {
+		desc: '卓越',
+		buyAmount: 100,
+		saleAmount: 0,
+	}, {
+		desc: '史诗',
+		buyAmount: 100,
+		saleAmount: 0,
+	}, {
+		desc: '神话',
+		buyAmount: 100,
+		saleAmount: 0,
+	}, {
+		desc: '传说',
+		buyAmount: 100,
+		saleAmount: 0,
+	}];
+
+	chrome.extension.sendMessage({ cmd: "get_config" }, function (response) {
 		var degreeConf = response.data;
-		if(degreeConf == '') {
+		if (degreeConf == '') {
 			degreeConf = defaultDegreeConf;
 		}
 		console.log("start get dogs....\n")
-		setInterval(function(){
-			getBaiduDogs(degreeConf);
+		var _this = this;
+		var interval = setInterval(function () {
+			getBaiduDogs(degreeConf, _this);
 		}, 3000);
 
-	}); 
-	
+	});
+
 }
